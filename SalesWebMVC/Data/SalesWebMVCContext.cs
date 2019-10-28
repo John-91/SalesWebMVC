89615f17ee72a8d43631d647e4393ea858094036
John-91 <jonvaladao91@gmail.com>
2019-10-28 17:14:26 -0300

    Adicionando outras entidades e Segunda Migração

--- SalesWebMVC/Data/SalesWebMVCContext.cs
+++ SalesWebMVC/Data/SalesWebMVCContext.cs
@@ -13,6 +13,8 @@ namespace SalesWebMVC.Models
         {
         }
 
-        public DbSet<SalesWebMVC.Models.Departaments> Departaments { get; set; }
+        public DbSet<Departaments> Departaments { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
